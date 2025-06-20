6c2ffa43559c4fe0d76668bbfc45a6512936522f
JayeshJha <jayeshrajjha@gmail.com>
2025-06-20 16:27:49 -0700

    Added parkour and created building. Added enemy.

---
+++ Adventure Game - Jayesh Jha/Assets/Collection.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Collection : MonoBehaviour
+{
+    public int TotalCollection;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter (Collision Craft){
+        Debug.Log("Hello");
+        if(Craft.gameObject.tag=="Collectable"){
+Destroy(Craft.gameObject);
+TotalCollection+=1;
+        }
+    }
+}
+
+
