0c4089eef79eed881d981e1fb81b7e82704d88dc
JayeshJha <jayeshrajjha@gmail.com>
2025-07-02 15:57:17 -0700

    added coins and made text for coin amounts.

--- Assets/Collection.cs
+++ Assets/Collection.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 public class Collection : MonoBehaviour
 {   public int TotalCollection;
+public TMP_Text CoinText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,8 +23,13 @@ public class Collection : MonoBehaviour
         if(Craft.gameObject.tag=="Collectable"){
 Destroy(Craft.gameObject);
 TotalCollection+=1;
-if(TotalCollection>=8){
-SceneManager.LoadScene ("Win");}
+CoinText.text=GameObject.FindGameObjectsWithTag("Collectable").Length.ToString()+" Remaining";
+if(TotalCollection>=8 && SceneManager.GetActiveScene().name=="SampleScene"){
+SceneManager.LoadScene ("Level2");}
+if(TotalCollection>=5 && SceneManager.GetActiveScene().name=="Level2"){
+SceneManager.LoadScene ("Win");    
+}
+
     }
 }
 }
