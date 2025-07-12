f8fa93e30e81c676dd554e4a25ce7563d597ae2d
JayeshJha <jayeshrajjha@gmail.com>
2025-07-11 17:56:54 -0700

    STUFF.

--- Assets/Collection.cs
+++ Assets/Collection.cs
@@ -2,43 +2,290 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
-public class Collection : MonoBehaviour
-{   public int TotalCollection;
-public TMP_Text CoinText;
-public float ctime;
-public TMP_Text TimerText;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-  
+using PlayFab.ClientModels;
+using System.Collections.Generic;
+using PlayFab;
+public class Collection: MonoBehaviour {
+  public int TotalCollection;
+  public TMP_Text CoinText;
+  public float ctime;
+  public TMP_Text TimerText;
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+
   {
-        TimerText.text="00:00";
-    }
+    TimerText.text = "00:00";
+    var request = new LoginWithCustomIDRequest {
+      CustomId = SystemInfo.deviceUniqueIdentifier,
+        CreateAccount = true
+    };
+    PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnFailure);
+  }
+  void OnSuccess(LoginResult Result) {
+    Debug.Log("Login Worked");
+  }
+  void OnFailure(PlayFabError Result) {
+    Debug.LogError("PlayFab login failed: " + Result.GenerateErrorReport());
+  }
+  // Update is called once per frame
+  void Update() {
+    ctime += Time.deltaTime;
+    int minute = (int) ctime / 60;
+    int second = (int) ctime % 60;
+    TimerText.text = minute.ToString() + ":" + second.ToString();
+  }
+  void OnCollisionEnter(Collision Craft) {
+    if (Craft.gameObject.tag == "Collectable") {
+      Destroy(Craft.gameObject);
+      TotalCollection += 1;
 
-    // Update is called once per frame
-    void Update()
-    {
-        ctime += Time.deltaTime;
-        int minute = (int)ctime/60;
-        int second = (int)ctime%60;
-        TimerText.text=minute.ToString()+":"+second.ToString();
-    }
-    void OnCollisionEnter (Collision Craft){
-        Debug.Log("Hello");
-        if(Craft.gameObject.tag=="Collectable"){
-Destroy(Craft.gameObject);
-TotalCollection+=1;
+      int remaining = GameObject.FindGameObjectsWithTag("Collectable").Length - 1;
+      CoinText.text = remaining.ToString() + " Remaining";
+      if (TotalCollection >= 8 && SceneManager.GetActiveScene().name == "SampleScene") {
+        points();
+        Invoke("Level1", 3);
+      }
+      if (TotalCollection >= 5 && SceneManager.GetActiveScene().name == "Level2") {
+        points();
+        Invoke("Level2", 3);
 
-int remaining=GameObject.FindGameObjectsWithTag("Collectable").Length-1;
-CoinText.text=remaining.ToString()+" Remaining";
-if(TotalCollection>=8 && SceneManager.GetActiveScene().name=="SampleScene"){
-SceneManager.LoadScene ("Level2");}
-if(TotalCollection>=5 && SceneManager.GetActiveScene().name=="Level2"){
-SceneManager.LoadScene ("Win");    
+      }
+
+    }
+  }
+  void Level1() {
+    SceneManager.LoadScene("Level2");
+    Debug.Log("Uploading Stats");
+  }
+  void Level2() {
+    SceneManager.LoadScene("Win");
+    Debug.Log("Uploading Stat");
+  }
+  void points() {
+    string TitleHighScore = "";
+    if (SceneManager.GetActiveScene().name == "SampleScene") {
+      TitleHighScore = "HighScoreLvl1";
+    }
+    if (SceneManager.GetActiveScene().name == "Level2") {
+      TitleHighScore = "HighScoreLvl2";
+    }
+PlayFabClientAPI.GetPlayerStatistics(new GetPlayerStatisticsRequest(),
+            result => {
+                foreach (var stat in result.Statistics)
+                {
+                    if (stat.StatisticName == TitleHighScore)
+                    {
+                      int HighScore=stat.Value; 
+                      if((int) ctime < HighScore){
+                        var request2 = new UpdatePlayerStatisticsRequest {
+      Statistics = new List < StatisticUpdate > {
+        new StatisticUpdate {
+          StatisticName = TitleHighScore,
+          Value = (int) ctime
+        }
+      }
+    };
+    PlayFabClientAPI.UpdatePlayerStatistics(request2,
+      result => Debug.Log("Score submitted!"),
+      error => Debug.LogError("Failed to submit score: " + error.GenerateErrorReport()));
+
+                      }              
+                    }
+                }
+            }
+            
+          ,
+          error=>
+          {
+            Debug.LogError(error.GenerateErrorReport());
+          }
+);
+    
+  }
 }
 
 
-    }
-}
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
